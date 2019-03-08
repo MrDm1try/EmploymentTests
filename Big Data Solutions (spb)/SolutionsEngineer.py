13e77e07dbc058579b595dcacca0eed77af67e1d
Dumitru Racicovschii <dmr@kamstrup.com>
2019-03-08 11:46:26 +0100

    Added solutions for BigDataSolutions

---
+++ Big Data Solutions (spb)/SolutionsEngineer.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+from csv import DictReader
+
+
+def timeStringToMilliseconds(time):
+    """
+    Doing manually to avoid imports
+    """
+    hours, minutes, seconds = time.split(":")
+    return int(hours) * 3600000 + int(minutes) * 60000 + int(float(seconds) * 1000)
+
+
+trades = defaultdict(list)
+with open('trades.csv', 'r') as f:
+    reader = DictReader(f, fieldnames=['time', 'price', 'size', 'exchange'])
+    for line in reader:
+        trades[line['exchange']].append(timeStringToMilliseconds(line['time']))
+
+answers = {}
+for exchange, timestamps in trades.items():
+    ans = 0
+    indexOfLastInWindow = 0
+
+    for i, timestamp in enumerate(timestamps):
+        while indexOfLastInWindow < len(timestamps) and timestamps[indexOfLastInWindow] - timestamps[i] < 1000:
+            indexOfLastInWindow += 1
+        if indexOfLastInWindow - i > ans:
+            ans = indexOfLastInWindow - i
+
+    answers[exchange] = ans
+
+for answer in sorted(answers.items(), key=lambda x: x[0]):
+    print(answer[1])
